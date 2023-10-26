03ff856296000572360272d36bf6ae7c00ad2f5d
venelinkolev <venelinkolev@gmail.com>
2023-10-26 17:50:38 +0300

    Header Page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,14 @@ import './Header.css';
 export default function Header() {
     return (
         <>
-            
+            <header className="header">
+                <div className="logo">
+                
+                </div>
+                <div className="nav-menu">
+                
+                </div>                
+            </header>
         </>
     )
 }
