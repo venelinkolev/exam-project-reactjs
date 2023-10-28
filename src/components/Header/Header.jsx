fc636d246ac9cc5b76b5bdf0741825130f25bb79
venelinkolev <venelinkolev@gmail.com>
2023-10-28 11:10:41 +0300

    Responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,12 @@ export default function Header() {
         <div className='logo'>
           <img src='/recipes.png' alt='Recipes Logo' />
         </div>
-        <div className='nav-menu'>
+        <div className='menu-icon'>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
+            <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
+          </svg>
+        </div>
+        <div className='nav-menu expand-menu'>
           <ul>
             <li>
               <a href='#'>Начало</a>
