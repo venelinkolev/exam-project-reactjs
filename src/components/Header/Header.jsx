72e9085d85f9fac002b9e38a50e369e70e08b841
venelinkolev <venelinkolev@gmail.com>
2023-10-28 11:22:06 +0300

    Header and Home Changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
             <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
           </svg>
         </div>
-        <div className='nav-menu expand-menu'>
+        <div className='nav-menu'>
           <ul>
             <li>
               <a href='#'>Начало</a>
