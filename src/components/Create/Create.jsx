5928941be9ab452eec6eb19c48bb67e8b0f4bd91
venelinkolev <venelinkolev@gmail.com>
2023-11-18 12:52:25 +0200

    CreateRecipe Function

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -6,12 +6,16 @@ import FormRecipe from '../FormRecipe/FormRecipe';
 export default function Create() {
   useTitleChange('Create');
 
+  function createRecipe(e) {
+    e.preventDefault();
+    console.log(e);
+  }
   return (
     <>
       <div className='create-container'>
         <div className='create-recipe'>
           <h1>Създай Рецепта</h1>
-          <form>
+          <form onSubmit={createRecipe}>
             <FormRecipe />
             <input type='submit' value='Създай' />
           </form>
