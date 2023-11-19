ce3e278324aed8427edb8bba02fc88d3b9745cec
venelinkolev <114242353+venelinkolev@users.noreply.github.com>
2023-11-19 17:00:35 +0200

    Login Form Create

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,42 @@
 import './Login.css';
 
 export default function Login() {
+
+  function loginUser(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
     <>
-      <h1>Login</h1>
+      <div className='login-container'>
+        <div className='login-user'>
+          <h1>Вход потребител</h1>
+          <form onSubmit={loginUser}>
+            <div className='login-form-container'>
+              <label htmlFor='recipeName'>Заглавие:</label>
+              <input
+                type='text'
+                id='recipeName'
+                name='recipeName'
+                placeholder='Наименование на рецептата'
+                // value={formValues.recipeName}
+                // onChange={changeFormHandler}
+              />
+              <label htmlFor='imageUrl'>Зареди снимка:</label>
+              <input
+                type='text'
+                id='imageUrl'
+                name='imageUrl'
+                placeholder='http://... или https://...'
+                // value={formValues.imageUrl}
+                // onChange={changeFormHandler}
+              />
+            </div>
+            <input type='submit' value='Създай' />
+          </form>
+        </div>
+      </div>
     </>
   );
 }
