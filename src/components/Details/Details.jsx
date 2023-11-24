787d97443878db9a3b0eebfcdcac8f7d538b305d
venelinkolev <venelinkolev@gmail.com>
2023-11-24 13:51:40 +0200

    Edit Component Finish

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -48,29 +48,44 @@ export default function Details() {
       <div className='details-container'>
         <h1>{recipe.recipeName}</h1>
         <div className='recipe-details container'>
-          <div className='recipe-details-image'>
-            <img src={recipe.imageUrl} alt={recipe.recipeName} />
+          <div className='image-table'>
+            <div className='recipe-details-image'>
+              <img src={recipe.imageUrl} alt={recipe.recipeName} />
+            </div>
+            <div className='recipe-details-table'>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Време за приготвяне</th>
+                    <th>Време за готвене</th>
+                    <th>Общо време</th>
+                    <th>Порции</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{recipe.prepTime}</td>
+                    <td>{recipe.cookTime}</td>
+                    <td>{recipe.totalTime}</td>
+                    <td>{recipe.servings}</td>
+                  </tr>
+                  <tr>
+                    <td>минути</td>
+                    <td>минути</td>
+                    <td>минути</td>
+                    <td>брой</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
           <div className='recipe-info'>
             <div className='recipe-details-ingredients'>
               <h2>Наичин на приготвяне(продукти)</h2>
               {/* {transform(recipe.ingredients).map((line, index) => (
-                <p key={index}>{line}</p>
+                <p key={index}>{line}</p> 
               ))} */}
               <p>{recipe.ingredients}</p>
-            </div>
-            <div className='recipe-details-tabel'>
-              <table>
-                <thead>
-                  <th>Време за приготвяне</th>
-                  <th>Време за готвене</th>
-                  <th>Общо време</th>
-                  <th>Порции</th>
-                </thead>
-                <tbody>
-                  <tr></tr>
-                </tbody>
-              </table>
             </div>
             <div className='recipe-details-btn'>
               <Link to={'/catalog'}>Каталог</Link>
