1dca28ffb8d6ff3b3fa15892a69c3e371b14f77d
venelinkolev <venelinkolev@gmail.com>
2023-11-24 09:58:25 +0200

    Recipe Details Component

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from 'react';
+import useTitleChange from '../../hooks/useTitleChange';
 import './Details.css';
+import { getRecipe } from '../../services/recipeServices';
+import { useParams } from 'react-router-dom';
 
 export default function Details() {
+  const [recipe, setRecipe] = useState({
+    recipeName: '',
+    imageUrl: '',
+    ingredients: '',
+    prepTime: '',
+    cookTime: '',
+    totalTime: '',
+    servings: '',
+  });
+
+  useTitleChange('Details');
+
+  const { recipeId } = useParams();
+
+  useEffect(() => {
+    getRecipe(recipeId)
+      .then((recipe) => {
+        console.log(recipe);
+        setRecipe(recipe[0]);
+      })
+      .catch((err) => console.log(err));
+  }, [recipeId]);
   return (
     <>
-      <h1>Details</h1>
+      <div className='details-container'>
+        <h1>{recipe.recipeName}</h1>
+        <div className='recipe-details'>
+          <div className='recipe-details-image'></div>
+          <div className='recipe-details-tabel'></div>
+          <div className='recipe-details-ingredients'></div>
+          <div className='recipe-details-btn'></div>
+        </div>
+      </div>
     </>
   );
 }
