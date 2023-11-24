0c0aef973d49c77253b719b3ade15abd701667c3
venelinkolev <venelinkolev@gmail.com>
2023-11-24 10:22:51 +0200

    Css Styles and Edit Btn Func

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from 'react';
 import useTitleChange from '../../hooks/useTitleChange';
 import './Details.css';
 import { getRecipe } from '../../services/recipeServices';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function Details() {
   const [recipe, setRecipe] = useState({
+    _id: '',
     recipeName: '',
     imageUrl: '',
     ingredients: '',
@@ -32,10 +33,18 @@ export default function Details() {
       <div className='details-container'>
         <h1>{recipe.recipeName}</h1>
         <div className='recipe-details'>
-          <div className='recipe-details-image'></div>
-          <div className='recipe-details-tabel'></div>
-          <div className='recipe-details-ingredients'></div>
-          <div className='recipe-details-btn'></div>
+          <div className='recipe-details-image'>
+            <img src={recipe.imageUrl} alt={recipe.recipeName} />
+          </div>
+          <div className='recipe-info'>
+            <div className='recipe-details-tabel'></div>
+            <div className='recipe-details-ingredients'></div>
+            <div className='recipe-details-btn'>
+              <Link>Каталог</Link>
+              <Link to={`/catalog/${recipe._id}/edit`}>Редактирай</Link>
+              <Link>Изтрий</Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
