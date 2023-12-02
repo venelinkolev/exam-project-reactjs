b0bb25ba3f13c6a6613124203f3b3173ec9c140e
venelinkolev <venelinkolev@gmail.com>
2023-12-02 12:32:33 +0200

    Spinner Details Page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import { UserContext } from '../../contexts/UserContext';
 import GoToTop from '../../util/GoToTop';
 import { ServerErrorHandlerContext } from '../../contexts/ServerErrorHandlerContext';
+import Spinner from '../Spinner/Spinner';
 
 export default function Details() {
   const [recipe, setRecipe] = useState({
@@ -78,58 +79,63 @@ export default function Details() {
   return (
     <>
       <div className='details-container'>
-        <h1>{recipe.recipeName}</h1>
-        <div className='recipe-details container'>
-          <div className='image-table'>
-            <div className='recipe-details-image'>
-              <img src={recipe.imageUrl} alt={recipe.recipeName} />
-            </div>
-            <div className='recipe-details-table'>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Време за приготвяне</th>
-                    <th>Време за готвене</th>
-                    <th>Общо време</th>
-                    <th>Порции</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{recipe.prepTime}</td>
-                    <td>{recipe.cookTime}</td>
-                    <td>{recipe.totalTime}</td>
-                    <td>{recipe.servings}</td>
-                  </tr>
-                  <tr>
-                    <td>минути</td>
-                    <td>минути</td>
-                    <td>минути</td>
-                    <td>брой</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div className='recipe-info'>
-            <div className='recipe-details-ingredients'>
-              <h2>Наичин на приготвяне(продукти)</h2>
-              {/* {transform(recipe.ingredients).map((line, index) => (
+        {recipe._id === '' && <Spinner />}
+        {recipe._id !== '' && (
+          <>
+            <h1>{recipe.recipeName}</h1>
+            <div className='recipe-details container'>
+              <div className='image-table'>
+                <div className='recipe-details-image'>
+                  <img src={recipe.imageUrl} alt={recipe.recipeName} />
+                </div>
+                <div className='recipe-details-table'>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Време за приготвяне</th>
+                        <th>Време за готвене</th>
+                        <th>Общо време</th>
+                        <th>Порции</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{recipe.prepTime}</td>
+                        <td>{recipe.cookTime}</td>
+                        <td>{recipe.totalTime}</td>
+                        <td>{recipe.servings}</td>
+                      </tr>
+                      <tr>
+                        <td>минути</td>
+                        <td>минути</td>
+                        <td>минути</td>
+                        <td>брой</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div className='recipe-info'>
+                <div className='recipe-details-ingredients'>
+                  <h2>Наичин на приготвяне(продукти)</h2>
+                  {/* {transform(recipe.ingredients).map((line, index) => (
                 <p key={index}>{line}</p> 
               ))} */}
-              <p>{recipe.ingredients}</p>
+                  <p>{recipe.ingredients}</p>
+                </div>
+                <div className='recipe-details-btn'>
+                  <Link to={'/catalog'}>Каталог</Link>
+                  {isOwnerRecipe && (
+                    <>
+                      <Link to={`/catalog/${recipe._id}/edit`}>Редактирай</Link>
+                      <Link onClick={deleteRecipe}>Изтрий</Link>
+                    </>
+                  )}
+                </div>
+              </div>
             </div>
-            <div className='recipe-details-btn'>
-              <Link to={'/catalog'}>Каталог</Link>
-              {isOwnerRecipe && (
-                <>
-                  <Link to={`/catalog/${recipe._id}/edit`}>Редактирай</Link>
-                  <Link onClick={deleteRecipe}>Изтрий</Link>
-                </>
-              )}
-            </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
       <GoToTop />
     </>
