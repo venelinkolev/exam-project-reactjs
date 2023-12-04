14fb906b6bcf06dc12c11d86118052f1b56a0b52
venelinkolev <venelinkolev@gmail.com>
2023-12-04 17:19:21 +0200

    Change Nav Menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -68,11 +68,6 @@ export default function Header() {
             <img src='/recipes.png' alt='Recipes Logo' />
           </Link>
         </div>
-        <div className='menu-icon' onClick={menuShowOrHide}>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
-            <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
-          </svg>
-        </div>
         <div
           className='nav-menu'
           style={showMenu ? { display: 'block' } : { display: 'none' }}
@@ -131,6 +126,11 @@ export default function Header() {
               </>
             )}
           </ul>
+        </div>
+        <div className='menu-icon' onClick={menuShowOrHide}>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
+            <path d='M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z' />
+          </svg>
         </div>
         {showProfile && (
           <div className='profile-modal'>
